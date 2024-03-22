54ffb46b1c55edba3c83e0c05537b3b4df1f0251
donmaleek <bamboloshari@gmail.com>
2024-03-22 13:07:08 -0700

    1. Install a package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"', # Ensures it's only installed if not already at the desired version
+}
+
